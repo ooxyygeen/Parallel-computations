b73287f32253b5240712ba98574ab17d0fef9851
oxygen <ooxyygen0@gmail.com>
2023-02-16 20:22:41 +0200

    update multithreaded solution

--- src/main/kotlin/task1/Solution.kt
+++ src/main/kotlin/task1/Solution.kt
@@ -7,7 +7,7 @@ package task1
  * diagonal if needed.
  */
 
-import java.util.Random
+import java.util.*
 import kotlin.concurrent.thread
 
 fun fillColumn(arr: Array<IntArray>, colIndex: Int) {
@@ -22,6 +22,14 @@ fun fillColumn(arr: Array<IntArray>, colIndex: Int) {
     }
 }
 
+fun fillColumn(arr: Array<IntArray>, startCol: Int, endCol: Int) {
+    val random = Random()
+    var indexMax = 0
+    for (j in startCol until endCol) {
+        fillColumn(arr, j)
+    }
+}
+
 fun printMatrix(matrix: Array<IntArray>) {
     for (i in matrix.indices) {
         for (j in matrix.indices) {
@@ -33,41 +41,46 @@ fun printMatrix(matrix: Array<IntArray>) {
 }
 
 fun main() {
-    val matrixSize = 128
+    val matrixSize = 10
+    val threadNumber = 4
     val matrix = Array(matrixSize) { IntArray(matrixSize) }
-//    val threads = mutableListOf<Thread>()
     val threads = ArrayList<Thread>()
 
-    val startTimeSingleThread = System.nanoTime()
+    val startTimeSingleThread = System.currentTimeMillis()
 
     for (j in 0 until matrixSize) {
         fillColumn(matrix, j)
     }
 
-    val endTimeSingleThread = System.nanoTime()
+    val endTimeSingleThread = System.currentTimeMillis()
 
     println("Single-thread")
     printMatrix(matrix)
 
-    val startTimeMultiThread = System.nanoTime()
+    val startTimeMultiThread = System.currentTimeMillis()
 
-    for (j in 0 until matrixSize) {
+    for (i in 0 until threadNumber) {
         val thread = thread {
-            fillColumn(matrix, j)
+            for (j in (matrixSize/threadNumber * i until
+                    if (i == threadNumber - 1) matrixSize else matrixSize / threadNumber * (i + 1))) {
+                fillColumn(matrix, j)
+            }
         }
         threads.add(thread)
     }
 
     for (thread in threads) { thread.join() }
 
-    val endTimeMultiThread = System.nanoTime()
+    val endTimeMultiThread = System.currentTimeMillis()
 
     println("\nMulti-thread")
     printMatrix(matrix)
 
+    println("Matrix size: $matrixSize\t Threads number: $threadNumber")
+
     val timeTakenSingleThread = endTimeSingleThread - startTimeSingleThread
-    println("\nTime taken for single-thread solution: $timeTakenSingleThread nanoseconds")
+    println("\nTime taken for single-thread solution: $timeTakenSingleThread milliseconds")
 
     val timeTakenMultiThread = endTimeMultiThread - startTimeMultiThread
-    println("Time taken for multi-thread solution:  $timeTakenMultiThread nanoseconds")
+    println("Time taken for multi-thread solution:  $timeTakenMultiThread milliseconds")
 }
